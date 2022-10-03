4f656edd1a937fafafba6a998e00051b257ada1f
MiguelRoiNava <83698242+MiguelRoiNava@users.noreply.github.com>
2022-10-03 16:39:54 +0800

    Add files via upload

---
+++ prog.php
@@ -0,0 +1,30 @@
+<html>
+<head><title>This is my first webpage</title></head>
+<body bgcolor = "blue"
+<p>This HTML will get delivered as is</p>
+<?php echo "<p>But this code is interpreted by PHP and turned into HTML</p>";
+    $j = 4;
+    $k = 4;
+    
+    $num1 = 6;
+    $num2 = 2;
+    $sum = $num1 + $num2;
+    $username = "Nava, Miguel Roi B.";    
+    $program = "Computer Engineering";
+    $string = "php programming";
+    $yearsection = "3rd year, 58043";
+    echo $username;
+    echo"<br>";
+    echo $yearsection;
+    echo"<br>";
+    echo $program;
+    echo"<br>";
+    echo "Today i will learn ".$string;
+    echo"<br>";
+    if($j===$k)
+
+//This is the snippet for php code 
+?>
+
+</body>
+</html>
